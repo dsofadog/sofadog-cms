759951856dbfa1d8f1f66306a6c80ea97997440c
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-18 11:47:33 +0530

    Added constant file to store constatnt data of cms

---
+++ utils/cms-constant.tsx
@@ -0,0 +1,70 @@
+export default class CmsConstant {
+    static Category = [
+        {
+            id: 1,
+            name: "LIFE & POLITICS",
+            value: 0,
+            color: "PURPLE"
+        },
+        {
+            id: 2,
+            name: "FASHION & ART",
+            value: 1,
+            color: "ORANGE"
+        },
+        {
+            id: 3,
+            name: "CELEBRITIES",
+            value: 2,
+            color: "PINK"
+        },
+        {
+            id: 4,
+            name: "ENTERTAINMENT",
+            value: 3,
+            color: "RED"
+        },
+        {
+            id: 5,
+            name: "NATURE",
+            value: 4,
+            color: "GREEN"
+        },
+        {
+            id: 6,
+            name: "SCIENCE",
+            value: 5,
+            color: "BLACK"
+        },
+        {
+            id: 7,
+            name: "BUSINESS & TECH",
+            value: 6,
+            color: "BLUE"
+        },
+        {
+            id: 8,
+            name: "SELF-IMPROVEMENT",
+            value: 7,
+            color: "YELLOW"
+        }
+    ]
+
+    static Tags = [
+        {
+            id: 1,
+            name: "Local Estonian Content",
+            value: "Local Estonian Content",
+        },
+        {
+            id: 2,
+            name: "NSFW",
+            value: "NSFW",
+        },
+        {
+            id: 3,
+            name: "NSFL",
+            value: "NSFL",
+        }
+    ]
+}
