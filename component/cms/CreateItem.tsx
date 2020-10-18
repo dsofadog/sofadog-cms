2a4a85be89010c61e2a5eea70de2d38f22c475ff
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-18 19:49:10 +0530

    Store title value on create item page

--- component/cms/CreateItem.tsx
+++ component/cms/CreateItem.tsx
@@ -7,6 +7,28 @@ const CreateItem = (props) => {
 
     const [openCategoryDropdown, setOpenCategoryDropdown] = useState(false);
     const toggleCateDropdown = () => { setOpenCategoryDropdown(!openCategoryDropdown) };
+
+    const [item,setItem] = useState({
+        title: '',
+        descriptions: [],
+        news_credits: [],
+        visual_credits: [],
+        tags: [],
+        category: ''
+    });
+
+    function handleChangeInput(e){
+        console.log(e.target.value)
+        setItem({
+            ...item,
+            title: e.target.value
+        });
+    }
+
+    function handleClickSingleDropdown(e){
+        
+    }
+
     
     return (
         <div className="w-full mx-auto">
@@ -28,11 +50,9 @@ const CreateItem = (props) => {
                             </div>
                             <div className="flex items-center mb-4">
                                 <div className="w-full">
-                                    <label htmlFor="about" className="block text-sm font-medium leading-5 text-gray-700">
-                                        Title
-                                            </label>
+                                    <label htmlFor="about" className="block text-sm font-medium leading-5 text-gray-700">Title</label>
                                     <div className="mt-1 rounded-md shadow-sm">
-                                        <textarea id="about" rows={2} className="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" placeholder="Enter Title"></textarea>
+                                        <textarea id="about" value={item.title} rows={2} onChange={(e) => handleChangeInput(e)} className="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" placeholder="Enter Title"></textarea>
                                     </div>
                                 </div>
                             </div>
@@ -177,24 +197,7 @@ const CreateItem = (props) => {
                         </div>
 
                         <div className="w-full md:w-1/5 relative z-10 bg-gray-100 border-l border-gray-200 p-2 rounded-lg rounded-l-none">
-                            <div className="h-full w-full flex items-start">
-                                <div className="w-full flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                                    <div className="text-center">
-                                        <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48">
-                                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                        </svg>
-                                        <p className="mt-1 text-sm text-gray-600">
-                                            <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition duration-150 ease-in-out">
-                                                Upload a file
-                                                    </button>
-                                                    or drag and drop
-                                                </p>
-                                        <p className="mt-1 text-xs text-gray-500">
-                                            MP4, MOV, WMV up to 500MB
-                                                </p>
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
 
                     </div>
