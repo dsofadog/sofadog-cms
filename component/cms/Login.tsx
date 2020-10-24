6364c7b7a817babfd635d03d65035af85ad150ea
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-24 13:33:50 +0530

    Login page bg changed

--- component/cms/Login.tsx
+++ component/cms/Login.tsx
@@ -9,13 +9,13 @@ const Login = () => {
 
     return (
         <>
-            <div className="min-h-screen flex items-center justify-center bg-gray-500 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="min-h-screen flex items-center justify-center sfd-bg-primary py-12 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-md w-full bg-white p-4 rounded-lg">
                     <div>
                         <img className="h-10 w-auto mx-auto" src="/color-logo.png" alt="So.Fa.Dog" />
                         <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
                             Sign in to your account
-                    </h2>
+                        </h2>
                     </div>
                     <form onSubmit={(e) => doLogin(e)} className="mt-8" method="POST">
                         <input type="hidden" name="remember" value="true" />
@@ -29,10 +29,10 @@ const Login = () => {
                         </div>
 
                         <div className="mt-6">
-                            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white sfd-btn-primary focus:outline-none transition duration-150 ease-in-out">
                                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                                    <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                                    <svg className="h-5 w-5 text-white group-hover:text-gray-100 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                     </svg>
                                 </span>
                                 Sign in
