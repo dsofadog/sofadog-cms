fa41fa79b5000c47e570a7b4b2c00e7ebc3e49cb
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-25 18:12:09 +0530

    changed cms api endpoint

--- utils/http-cms.tsx
+++ utils/http-cms.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://cms-int.so.fa.dog/",
+    baseURL: "https://v.so.fa.dog",
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
