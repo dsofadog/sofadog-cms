fa66d28680cf66941415176af296b7d069671bf2
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-30 21:00:47 +0530

    Dropdown for comment

--- component/cms/Comment.tsx
+++ component/cms/Comment.tsx
@@ -2,7 +2,7 @@ import { config as f_config, library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import dynamic from "next/dynamic";
 import { LayoutContext } from '../../contexts';
 //import Editor from './Editor';
@@ -11,10 +11,10 @@ f_config.autoAddCss = false;
 library.add(fas, fab);
 
 const Editor = dynamic(
-  () => {
-    return import("./Editor");
-  },
-  { ssr: false }
+    () => {
+        return import("./Editor");
+    },
+    { ssr: false }
 );
 
 const Comment = (props) => {
@@ -23,6 +23,10 @@ const Comment = (props) => {
     const [type, setType] = useState('view');
     const [isTrix, setIsTrix] = useState(false);
     const [body, setBody] = useState('');
+    const [openActionDropdown, setOpenActionDropdown] = useState(false);
+    const toggleActionDropdown = () => { setOpenActionDropdown(!openActionDropdown) };
+    const actionWrapperRef = useRef(null);
+    useOutsideAlerter(actionWrapperRef);
 
     useEffect(() => {
         if (props.comment) {
@@ -40,6 +44,26 @@ const Comment = (props) => {
         };
     });
 
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+            function handleClickOutside(event) {
+
+                if (ref.current && !ref.current.contains(event.target)) {
+                    if (ref.current.dataset.id === "action") {
+                        setOpenActionDropdown(false);
+                    }
+                }
+            }
+
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                // Unbind the event listener on clean up
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
     const escFunction = useCallback((event) => {
         if (event.keyCode === 27) {
             setIsTrix(false);
@@ -50,9 +74,11 @@ const Comment = (props) => {
         }
     }, []);
 
-    function submitComment(){
-        console.log("comment body:",body);
+    function submitComment() {
+        console.log("comment body:", body);
     }
+
+    
 
     return (
         <div className="w-full flex">
@@ -64,9 +90,25 @@ const Comment = (props) => {
             <div className="w-full -ml-2 bg-gray-100 px-2 py-0.5 rounded-2xl">
                 {type === 'view' ?
                     <>
-                        <div className="w-full text-sm text-gray-600 flex justify-end space-x-2">
+                        <div ref={actionWrapperRef} data-id="action" className="relative w-full text-sm text-gray-600 flex justify-end space-x-2">
                             <span>{comment?.created}</span>
-                            <FontAwesomeIcon className="w-5 text-gray-600 cursor-pointer" icon={['fas', 'ellipsis-h']} />
+                            <FontAwesomeIcon onClick={() => toggleActionDropdown()} className="w-5 text-gray-600 cursor-pointer" icon={['fas', 'ellipsis-h']} />
+                            {openActionDropdown && (
+                                <div className="origin-top-left absolute right-0 mt-5 w-40 rounded-md shadow-lg z-20">
+                                    <div className="rounded-md bg-blue-600 shadow-xs">
+                                        <div className="" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                            <a href={void (0)} className="flex space-x-2 rounded-t-md cursor-pointer block px-4 py-1 text-sm font-semibold leading-5 text-white hover:bg-blue-700 focus:outline-none" role="menuitem">
+                                                <FontAwesomeIcon className="w-3" icon={['fas', 'pencil-alt']} />
+                                                <span>Edit</span>
+                                            </a>
+                                            <a href={void (0)} className="flex space-x-2 rounded-b-md cursor-pointer block px-4 py-1 text-sm font-semibold leading-5 text-white hover:bg-blue-700 focus:outline-none" role="menuitem">
+                                                <FontAwesomeIcon className="w-3" icon={['fas', 'trash-alt']} />
+                                                <span>Delete</span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                         <div className="w-full space-y-1 px-2 pb-4">
                             <div>
