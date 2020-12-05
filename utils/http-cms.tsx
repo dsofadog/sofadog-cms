afa227e769f3b7d9a1874add7b5cde2af0c738ae
mak1986 <mak.jacobsen@gmail.com>
2020-12-05 23:26:51 +0700

    [fix] env files not working

--- utils/http-cms.tsx
+++ utils/http-cms.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://v-int.so.fa.dog",
+    baseURL: process.env.API_URL,
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
