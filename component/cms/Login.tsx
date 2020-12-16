a8884f52474e7ed4ebcb3a7548e39116a6716b47
mak1986 <mak.jacobsen@gmail.com>
2020-12-17 01:41:04 +0700

    [3286607282][fix][high] Auto-fill login doesn't work, you still have to type it manually everytime

--- component/cms/Login.tsx
+++ component/cms/Login.tsx
@@ -1,10 +1,24 @@
 import Router from "next/router";
 import React, { useContext, useEffect, useState } from "react";
-import _ from 'lodash'
-import { resolve6 } from "dns";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from 'yup'
 
 import { LayoutContext } from "contexts";
 import HttpCms from "utils/http-cms";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type Inputs = {
+  email: string;
+  password: string
+}
+
+const schema = yup.object().shape({
+  email: yup.string().required().label('Email'),
+  password: yup.string().required().label('Password')
+})
+
 
 const Login = () => {
   const {
@@ -17,17 +31,21 @@ const Login = () => {
     appState,
     currentUserState,
     setCurrentUserState,
-    userIsSuperAdmin, 
+    userIsSuperAdmin,
     setUserIsSuperAdmin,
     setSessionStorage,
     getSessionStorage,
     redirectUrl,
-    setRedirectUrl,
-    notify
+    setRedirectUrl
   } = useContext(LayoutContext);
 
-  const [userInfo, setUserInfo] = useState({email: '', password: ''});
-  const tempData=[];
+  const { register, handleSubmit, errors } = useForm<Inputs>({
+    resolver: yupResolver(schema)
+  })
+
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false);
+  const tempData = [];
 
   useEffect(() => {
     // setUserInfo({
@@ -40,36 +58,27 @@ const Login = () => {
     // })
   }, []);
 
-  function handleChangeInput(e) {
-    let value = e.target.value;
-    let name = e.target.name;
-    setUserInfo({
-      ...userInfo,
-      [name]: value,
-    });
-  }
+  const currentUserRoleManagement = async (data) => {
+    await manageuser(data);
+    //Router.push("/cms");
+    console.log(redirectUrl, "redirectUrl", redirectUrl != '');
 
-  const  currentUserRoleManagement = async (data) => {
-     await  manageuser(data);    
-     //Router.push("/cms");
-     console.log(redirectUrl,"redirectUrl",redirectUrl!='');
-
-     if(redirectUrl !=''){
+    if (redirectUrl != '') {
       Router.push(
         '/cms/[item_id]',
         '/cms/' + redirectUrl);
-        setRedirectUrl('');
-      
-    }else{
+      setRedirectUrl('');
+
+    } else {
       //setRedirectUrl('');
       Router.push("/cms");
     }
- 
-    
+
+
   };
 
-  function manageuser(data){
-    data?.user?.admin_roles.forEach(function (item) { 
+  function manageuser(data) {
+    data?.user?.admin_roles.forEach(function (item) {
       rolesAdded(item.id);
       //stateAdded(item.id);     
     });
@@ -84,47 +93,50 @@ const Login = () => {
   //     let data = appState[role]; 
   //     if(Array.isArray(currentUserState) && currentUserState.length){     
   //       setCurrentUserState(currentUserState => [...currentUserState, data]);
-       
+
   //   }else{
   //      let data = [];
   //      data.push(appState[role]);
   //      setCurrentUserState(data);
-     
+
   //   }
-      
+
 
   //   }
   // };
 
   const rolesAdded = (role) => {
-      if(role=="super_admin"){         
-        setUserIsSuperAdmin(1);
-      }
-    for (const [key, value] of Object.entries(appAction)) {   
-       // console.log(key, value,role);    
-        if(key==role){   
-            appAction[key].forEach(function(value) {
-              setCurrentUserAction(currentUserAction => [...currentUserAction, value]);  
-              
-          });
+    if (role == "super_admin") {
+      setUserIsSuperAdmin(1);
+    }
+    for (const [key, value] of Object.entries(appAction)) {
+      // console.log(key, value,role);    
+      if (key == role) {
+        appAction[key].forEach(function (value) {
+          setCurrentUserAction(currentUserAction => [...currentUserAction, value]);
 
-        }
+        });
+
       }
+    }
   };
 
-  const doLogin = (e) => {
-    // setLoading(true);
-    e.preventDefault();
-    HttpCms.post("/admin_user/login", userInfo)
+  const submit = (data: Inputs) => {
+
+    setIsLoading(true)
+
+    const { email, password } = schema.cast(data)
+
+    HttpCms.post("/admin_user/login", { email, password })
       .then((response) => {
-        if (response.data.token != "") {         
+        if (response.data.token != "") {
           let firstName = response.data.user.first_name;
           let lastName = response.data.user.last_name;
           response.data.displayName = firstName.charAt(0) + lastName.charAt(0);
           setAppUserInfo(response.data);
-          setSessionStorage('user_info',response.data);          
+          setSessionStorage('user_info', response.data);
           currentUserRoleManagement(response.data);
-          if(response.data.user.on_shift == null){
+          if (response.data.user.on_shift == null) {
             checkShiftSatus(response.data);
           }
           // let dummyData = {
@@ -145,39 +157,40 @@ const Login = () => {
           //   token: "a249b9a0-20a7-11eb-a957-19c7c71ee4c2",
           // };
           // currentUserRoleManagement(dummyData);
-          
-          
+
+
         } else {
-          notify('danger', 'on_shift not null, line 151')
           alert("Something wrong !!");
         }
+        setError(null)
       })
-      .catch((e) => {
-        if(e.response.data && e.response.data.error_backtrace){
-          e.response.data.error_backtrace = _.truncate(e.response.data.error_backtrace, {length: 500})
+      .catch((err) => {
+        if (err.response?.data?.error_message) {
+          const errorMessage = [
+            'AuthenticationError',
+            'Expected 1 results; got 0'
+          ].includes(err.response.data.error_message) ? 'Wrong credentials' : 'Something went wrong'
+
+          setError(errorMessage)
+          // setLoading(false);
         }
-        notify('danger', 'line: 159, username: ' + userInfo.email +' password: ' + userInfo.password.replace(/./g,'*') + ' '+ JSON.stringify(e.response.data))
-        // setLoading(false);
+
       })
       .finally(() => {
-        // setLoading(false);
+        setIsLoading(false);
       });
   };
-  function checkShiftSatus(appUserInfo){
+  function checkShiftSatus(appUserInfo) {
     HttpCms.patch(`/admin_user/${appUserInfo.user.email}/toggle_shift?token=${appUserInfo.token}`)
       .then((response) => {
-        if (response.data.user.on_shift != null) {         
+        if (response.data.user.on_shift != null) {
           //alert("shift api call when first time login");
         } else {
-          notify('danger', 'on_shift not null, line 169')
           alert("Something wrong !!");
         }
       })
       .catch((e) => {
-        if(e.response.data && e.response.data.error_backtrace){
-          e.response.data.error_backtrace = _.truncate(e.response.data.error_backtrace, {length: 500})
-        }
-        notify('danger', 'line: 180, username: ' + userInfo.email +' password: ' + userInfo.password.replace(/./g,'*') + ' '+ JSON.stringify(e.response.data))
+
         setLoading(false);
       })
       .finally(() => {
@@ -200,41 +213,73 @@ const Login = () => {
               Sign in to your account
             </h2>
           </div>
-          <form onSubmit={(e) => doLogin(e)} className="mt-8" method="POST">
+
+          {(error || errors.email || errors.password) && <div className="rounded-md bg-red-50 p-4 mt-5">
+            <div className="text-sm text-red-700">
+              <ul className="list-disc pl-5 space-y-1">
+                {errors.email && <li className="flex">
+                  <span className="flex-shrink-0">
+                    <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                  <span className="ml-3">{errors.email.message}.</span></li>}
+                {errors.password && <li className="flex">
+                  <span className="flex-shrink-0">
+                    <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                  <span className="ml-3">{errors.password.message}.</span></li>}
+                {error && <li className="flex">
+                  <span className="flex-shrink-0">
+                    <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                  <span className="ml-3">{error}.</span></li>}
+              </ul>
+            </div>
+          </div>}
+
+
+          <form onSubmit={handleSubmit(submit)} className="mt-8" >
             <input type="hidden" name="remember" value="true" />
             <div className="rounded-md shadow-sm">
+
               <div>
                 <input
+                  disabled={isLoading}
+                  ref={register}
                   aria-label="Email address"
                   name="email"
-                  value={userInfo?.email}
                   type="email"
-                  onChange={(e) => handleChangeInput(e)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  className={(isLoading ? 'cursor-not-allowed disabled:opacity-50 ' : '') + 'appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5'}
                   placeholder="Email address"
                 />
               </div>
               <div className="-mt-px">
                 <input
+                  disabled={isLoading}
+                  ref={register}
                   aria-label="Password"
                   name="password"
-                  value={userInfo?.password}
                   type="password"
-                  onChange={(e) => handleChangeInput(e)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  className={(isLoading ? 'cursor-not-allowed disabled:opacity-50 ' : '') + 'appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5'}
                   placeholder="Password"
                 />
               </div>
+
+
             </div>
 
             <div className="mt-6">
               <button
+                disabled={isLoading}
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white sfd-btn-primary focus:outline-none transition duration-150 ease-in-out"
+                className={(isLoading ? 'cursor-not-allowed disabled:opacity-50 ' : '') + 'group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white sfd-btn-primary focus:outline-none transition duration-150 ease-in-out'}
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                {!isLoading && <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <svg
                     className="h-5 w-5 text-white group-hover:text-gray-100 transition ease-in-out duration-150"
                     fill="currentColor"
@@ -246,8 +291,10 @@ const Login = () => {
                       clipRule="evenodd"
                     />
                   </svg>
-                </span>
-                Sign in
+                </span>}
+                {isLoading && <FontAwesomeIcon className="animate-spin h-5 w-5 mr-3" icon={['fas', 'spinner']} />}
+
+                {!isLoading && 'Sign in'}
               </button>
             </div>
           </form>
